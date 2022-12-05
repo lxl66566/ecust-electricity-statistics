9c5a133f26ea91ce3db30097d712592a9836338a
lxl66566 <18259734087@163.com>
2022-12-05 13:06:00 +0800

    1.1.0 use requests instead of playwright

--- main.py
+++ main.py
@@ -1,32 +1,48 @@
 from contextlib import suppress
-from playwright.sync_api import sync_playwright
 from pathlib import Path
-import json,datetime,os
+import json,datetime,os,requests,re
 
-with sync_playwright() as playwright:
-    browser = playwright.chromium.launch(headless=True)
-    context = browser.new_context(
-        user_agent='Mozilla/5.0 (Linux; U; Android 4.1.2; zh-cn; Chitanda/Akari) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30 MicroMessenger/6.0.0.58_r884092.501 NetType/WIFI'
-    )
-    page = context.new_page()
-    url = os.environ.get('URL').strip()
-    page.goto(url)
-    remain = float(page.locator('text=/\d+\.\d+度/i').inner_text().rstrip("度"))
-    
-    originstring = '[]'
-    data = []
-    date = datetime.datetime.now().strftime("%Y-%m-%d")
+# from playwright.sync_api import sync_playwright
+# with sync_playwright() as playwright:
+#     browser = playwright.chromium.launch(headless=True)
+#     context = browser.new_context(
+#         user_agent='Mozilla/5.0 (Linux; U; Android 4.1.2; zh-cn; Chitanda/Akari) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30 MicroMessenger/6.0.0.58_r884092.501 NetType/WIFI'
+#     )
+#     page = context.new_page()
+#     url = os.environ.get('URL').strip()
+#     page.goto(url)
+#     remain = float(page.locator('text=/\d+\.\d+度/i').inner_text().rstrip("度"))
 
-    with suppress(FileNotFoundError):
-        with open("data.js",'r',encoding='utf-8') as f:
-            originstring = f.read().lstrip("data=")
+url = os.environ.get('URL').strip()
+header = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Connection': 'keep-alive',
+    'Host': 'yktyd.ecust.edu.cn',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Linux; U; Android 4.1.2; zh-cn; Chitanda/Akari) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30 MicroMessenger/6.0.0.58_r884092.501 NetType/WIFI'
+}
+response = requests.get(url, headers=header)
 
-    data = json.loads(originstring)
-    if len(data) != 0 and date in data[-1].values():
-        data[-1]['kWh'] = remain
-    else:
-        data.append({"time" : date, "kWh" : remain})
-    
-    originstring = json.dumps(data,indent=4, ensure_ascii=False)
-    Path("data.js").write_text("data=" + originstring)
-    
+remain = float(re.findall((r"(\d+(\.\d+)?)度"),response.text)[0][0])
+originstring = '[]'
+data = []
+date = datetime.datetime.now().strftime("%Y-%m-%d")
+
+with suppress(FileNotFoundError):
+    with open("data.js",'r',encoding='utf-8') as f:
+        originstring = f.read().lstrip("data=")
+
+data = json.loads(originstring)
+if len(data) != 0 and date in data[-1].values():
+    data[-1]['kWh'] = remain
+else:
+    data.append({"time" : date, "kWh" : remain})
+
+originstring = json.dumps(data,indent=4, ensure_ascii=False)
+Path("data.js").write_text("data=" + originstring)
